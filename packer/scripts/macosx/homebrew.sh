a4f32f19f223abfbde39ff9bf3e154850fc6b0c1
Matthew Ahrenstein <matt@ahrenstein.com>
2016-07-21 16:47:42 -0400

    El Capitan is now 10.11.6

--- packer/scripts/macosx/homebrew.sh
+++ packer/scripts/macosx/homebrew.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# Fix a bug in the 10.11.6 installation
+/bin/mkdir -p /Users/vagrant/Library/Caches
+
 #Silently install homebrew
 yes '' | /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
